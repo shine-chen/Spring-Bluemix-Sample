079f1ef0c65fcb10c2679bb93089d884983f6542
Shine Chen <hsiehwei029@gmail.com>
2015-12-16 16:02:23 +0800

    Initial commit.

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,25 @@
+package hello;
+
+/**
+ * Created by shinechen on 12/14/15.
+ */
+public class Greeting {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
